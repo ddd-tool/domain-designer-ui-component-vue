9e390d8e004e6229870573bd8d9b7432d0d6b2f4
AlphaFoxz <841958335@qq.com>
2024-12-14 19:43:10 +0800

    wip

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,7 @@
 import Aura from '@primevue/themes/aura'
 import PrimeVue from 'primevue/config'
+import Tooltip from 'primevue/tooltip'
 
 import Ui from './UI.vue'
 
-export { Ui, PrimeVue, Aura }
+export { Ui, PrimeVue, Aura, Tooltip }
