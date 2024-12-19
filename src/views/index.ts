bfa54fae12df64c0f51e56e0a79f8905e619f7e4
AlphaFoxz <841958335@qq.com>
2024-12-19 20:33:04 +0800

    tsconfig

--- src/views/index.ts
+++ src/views/index.ts
@@ -1,47 +1,91 @@
-import type { DomainDesigner } from '@ddd-tool/domain-designer-core'
+import { DomainDesigner, isDomainDesigner } from '@ddd-tool/domain-designer-core'
 
-const data: Record<string, DomainDesigner> = {
-  user: (await import('F:/idea_projects/oneboot/.sdk/design/用户聚合.ts')).default,
-  'design-zh': (await import('./design-zh')).default,
-  'design-en': (await import('./design-en')).default,
-  'design-en2': (await import('./design-en')).default,
-  'design-en3': (await import('./design-en')).default,
-  'design-en4': (await import('./design-en')).default,
-  'design-en5': (await import('./design-en')).default,
-  'design-en6': (await import('./design-en')).default,
-  'design-en7': (await import('./design-en')).default,
-  'design-en8': (await import('./design-en')).default,
-  'design-en9': (await import('./design-en')).default,
-  'design-en0': (await import('./design-en')).default,
-  'design-en11': (await import('./design-en')).default,
-  'design-en22': (await import('./design-en')).default,
-  'design-en33': (await import('./design-en')).default,
-  'design-en44': (await import('./design-en')).default,
-  'design-en55': (await import('./design-en')).default,
-  'design-en66': (await import('./design-en')).default,
-  'design-en77': (await import('./design-en')).default,
-  'design-en88': (await import('./design-en')).default,
-  'design-en99': (await import('./design-en')).default,
-  'design-en00': (await import('./design-en')).default,
-  'design-en111': (await import('./design-en')).default,
-  'design-en222': (await import('./design-en')).default,
-  'design-en333': (await import('./design-en')).default,
-  'design-en444': (await import('./design-en')).default,
-  'design-en555': (await import('./design-en')).default,
-  'design-en666': (await import('./design-en')).default,
-  'design-en777': (await import('./design-en')).default,
-  'design-en888': (await import('./design-en')).default,
-  'design-en999': (await import('./design-en')).default,
-  'design-en000': (await import('./design-en')).default,
-  'design-en1111': (await import('./design-en')).default,
-  'design-en2222': (await import('./design-en')).default,
-  'design-en3333': (await import('./design-en')).default,
-  'design-en4444': (await import('./design-en')).default,
-  'design-en5555': (await import('./design-en')).default,
-  'design-en6666': (await import('./design-en')).default,
-  'design-en7777': (await import('./design-en')).default,
-  'design-en8888': (await import('./design-en')).default,
-  'design-en9999': (await import('./design-en')).default,
+const __d2 = (await import('./design-zh')).default
+const __d3 = (await import('./design-en')).default
+// 'design-en': (await import('./design-en')).default,
+// 'design-en2': (await import('./design-en')).default,
+// 'design-en3': (await import('./design-en')).default,
+// 'design-en4': (await import('./design-en')).default,
+// 'design-en5': (await import('./design-en')).default,
+// 'design-en6': (await import('./design-en')).default,
+// 'design-en7': (await import('./design-en')).default,
+// 'design-en8': (await import('./design-en')).default,
+// 'design-en9': (await import('./design-en')).default,
+// 'design-en0': (await import('./design-en')).default,
+// 'design-en11': (await import('./design-en')).default,
+// 'design-en22': (await import('./design-en')).default,
+// 'design-en33': (await import('./design-en')).default,
+// 'design-en44': (await import('./design-en')).default,
+// 'design-en55': (await import('./design-en')).default,
+// 'design-en66': (await import('./design-en')).default,
+// 'design-en77': (await import('./design-en')).default,
+// 'design-en88': (await import('./design-en')).default,
+// 'design-en99': (await import('./design-en')).default,
+// 'design-en00': (await import('./design-en')).default,
+// 'design-en111': (await import('./design-en')).default,
+// 'design-en222': (await import('./design-en')).default,
+// 'design-en333': (await import('./design-en')).default,
+// 'design-en444': (await import('./design-en')).default,
+// 'design-en555': (await import('./design-en')).default,
+// 'design-en666': (await import('./design-en')).default,
+// 'design-en777': (await import('./design-en')).default,
+// 'design-en888': (await import('./design-en')).default,
+// 'design-en999': (await import('./design-en')).default,
+// 'design-en000': (await import('./design-en')).default,
+// 'design-en1111': (await import('./design-en')).default,
+// 'design-en2222': (await import('./design-en')).default,
+// 'design-en3333': (await import('./design-en')).default,
+// 'design-en4444': (await import('./design-en')).default,
+// 'design-en5555': (await import('./design-en')).default,
+// 'design-en6666': (await import('./design-en')).default,
+// 'design-en7777': (await import('./design-en')).default,
+// 'design-en8888': (await import('./design-en')).default,
+// 'design-en9999': (await import('./design-en')).default,
+
+const data: Record<string, DomainDesigner> = {}
+
+if (isDomainDesigner(__d2)) {
+  data['design-zh'] = __d2
+}
+if (isDomainDesigner(__d3)) {
+  data['design-en1'] = __d3
+  data['design-en2'] = __d3
+  data['design-en3'] = __d3
+  data['design-en4'] = __d3
+  data['design-en5'] = __d3
+  data['design-en6'] = __d3
+  data['design-en7'] = __d3
+  data['design-en8'] = __d3
+  data['design-en9'] = __d3
+  data['design-en10'] = __d3
+  data['design-en11'] = __d3
+  data['design-en12'] = __d3
+  data['design-en13'] = __d3
+  data['design-en14'] = __d3
+  data['design-en15'] = __d3
+  data['design-en16'] = __d3
+  data['design-en17'] = __d3
+  data['design-en18'] = __d3
+  data['design-en19'] = __d3
+  data['design-en20'] = __d3
+  data['design-en21'] = __d3
+  data['design-en22'] = __d3
+  data['design-en23'] = __d3
+  data['design-en24'] = __d3
+  data['design-en25'] = __d3
+  data['design-en26'] = __d3
+  data['design-en27'] = __d3
+  data['design-en28'] = __d3
+  data['design-en29'] = __d3
+  data['design-en30'] = __d3
+  data['design-en31'] = __d3
+  data['design-en32'] = __d3
+  data['design-en33'] = __d3
+  data['design-en34'] = __d3
+  data['design-en35'] = __d3
+  data['design-en36'] = __d3
+  data['design-en37'] = __d3
+  data['design-en38'] = __d3
 }
 
 export default data
